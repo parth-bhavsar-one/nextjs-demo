6397b00ed4edfb6fb34027178b2d1c4758210e7b
MansiSenjaliya <mansi.senjaliya@estatic-infotech.com>
2022-05-04 15:04:47 +0530

    Design SignUp & SignIn Form.

--- component/form.tsx
+++ component/form.tsx
@@ -1,7 +1,132 @@
-import React from "react";
+import { useFormik, FormikProps } from "formik";
+interface FormValues {
+  email: string;
+  password: string;
+  fullName: string;
+  dob: string;
+}
 
-const form = () => {
-  return <div></div>;
+interface OtherProps {
+  type: string;
+  onFormSubmit: () => void;
+}
+
+const Form: any = (props: any & OtherProps & FormikProps<FormValues>) => {
+  const {
+    initialValuesOfSignIn,
+    initialValuesOfSignUp,
+    type,
+    signInSchema,
+    signUpSchema,
+    onFormSubmit,
+  } = props;
+  const formik:any = useFormik({
+    initialValues:
+      type === "SignIn" ? initialValuesOfSignIn : initialValuesOfSignUp,
+    validationSchema: type === "SignIn" ? signInSchema : signUpSchema,
+    onSubmit: async (values) => {
+      onFormSubmit();
+    },
+  });
+  return (
+    <div className="font-sans antialiased bg-grey-lightest">
+      <div className="w-full bg-grey-lightest">
+        <div className="container mx-auto py-8">
+          <div className="w-5/6 lg:w-1/2 mx-auto bg-white rounded shadow">
+            <div className="py-4 px-8 text-black text-xl border-b border-grey-lighter">
+              {type}
+            </div>
+            <div className="py-4 px-8">
+              {type === "SignUp" ? (
+                  <div className="mb-4">
+                    <label className="block text-grey-darker text-sm font-bold mb-2">
+                      Full Name
+                    </label>
+                    <input
+                      className="appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                      name="fullName"
+                      placeholder="Enter FullName"
+                      onChange={formik.handleChange}
+                      value={formik.values.fullName}
+                    />
+                    {formik.touched.fullName && formik.errors.fullName ? (
+                      <span className="text-red-500 text-sm">
+                        {formik.errors.fullName}
+                      </span>
+                    ) : null}
+                  </div>
+              ) : null}
+                <div className="mb-4">
+                  <label className="block text-grey-darker text-sm font-bold mb-2">
+                    Email Address
+                  </label>
+                  <input
+                    className="appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                    name="email"
+                    placeholder="Enter email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                  />
+                  {formik.touched.email && formik.errors.email ? (
+                    <span className="text-red-500 text-sm">
+                      {formik.errors.email}
+                    </span>
+                  ) : null}
+                </div>
+                {type === "SignIn" ? (
+                  <div className="mb-4">
+                    <label className="block text-grey-darker text-sm font-bold mb-2">
+                      Password
+                    </label>
+                    <input
+                      className="appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                      name="password"
+                      placeholder="Enter password"
+                      onChange={formik.handleChange}
+                      value={formik.values.password}
+                    />
+                    {formik.touched.password && formik.errors.password ? (
+                      <span className="text-red-500 text-sm">
+                        {formik.errors.password}
+                      </span>
+                    ) : null}
+                  </div>
+                ) : null}
+                {type === "SignUp" ? (
+                  <div className="mb-4">
+                    <label className="block text-grey-darker text-sm font-bold mb-2">
+                      Date of Birth
+                    </label>
+                    <input
+                      className="appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                      name="dob"
+                      type="date"
+                      placeholder="Enter Date Of Birth"
+                      onChange={formik.handleChange}
+                      value={formik.values.dob}
+                    />
+                    {formik.touched.dob && formik.errors.dob ? (
+                      <span className="text-red-500 text-sm">
+                        {formik.errors.dob}
+                      </span>
+                    ) : null}
+                  </div>
+                ) : null}
+                <div className="flex items-center justify-between mt-8">
+                  <button
+                    type="submit"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => formik.handleSubmit()}
+                  >
+                    {type === "SignIn" ? "Login" : "Register"}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
 };
 
-export default form;
+export default Form;
--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,63 +1,38 @@
 import type { NextPage } from "next";
 import * as yup from "yup";
-import { useFormik } from "formik";
+import Form from "../../component/form";
+
+interface InitialProps {
+  email: string;
+  password: string;
+}
 
 const Signin: NextPage = () => {
-
-	const formik = useFormik({
-		initialValues: {
-			email: "",
-			password: "",
-		},
-		validationSchema: yup.object().shape({
-			email: yup.string().email("Please enter a valid email").required("This field is required."),
-			password: yup.string().required("This field is required."),
-		}),
-		onSubmit: async (values) => {
-			console.log("Loggedin successfully.", values);
-		},
-	});
-
-	return (
-		<div className="font-sans antialiased bg-grey-lightest">
-			<div className="w-full bg-grey-lightest">
-				<div className="container mx-auto py-8">
-					<div className="w-5/6 lg:w-1/2 mx-auto bg-white rounded shadow">
-						<div className="py-4 px-8 text-black text-xl border-b border-grey-lighter">Login Here</div>
-						<div className="py-4 px-8">
-							<div className="mb-4">
-								<label className="block text-grey-darker text-sm font-bold mb-2">Email Address</label>
-								<input
-									className="appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-									name="email"
-									placeholder="Enter email"
-									onChange={formik.handleChange}
-									value={formik.values.email}
-								/>
-                {formik.touched.email && formik.errors.email ? <span className="text-red-500 text-sm">{formik.errors.email}</span> : null}
-							</div>
-							<div className="mb-4">
-								<label className="block text-grey-darker text-sm font-bold mb-2">Password</label>
-								<input
-									className="appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-									name="password"
-									placeholder="Enter password"
-									onChange={formik.handleChange}
-									value={formik.values.password}
-								/>
-								{formik.touched.password && formik.errors.password ? <span className="text-red-500 text-sm">{formik.errors.password}</span> : null}
-							</div>
-							<div className="flex items-center justify-between mt-8">
-								<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => formik.handleSubmit()}>
-									Login
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const initialValuesOfSignIn: InitialProps = { email: "", password: "" };
+  const signInSchema = yup.object({
+    email: yup
+      .string()
+      .email("Please enter a valid email")
+      .required("Email is required."),
+    password: yup
+      .string()
+      .required("Password is required.")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+        "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+      ),
+  });
+  const handleButtonClick = () => {
+    console.log("Loggedin successfully.");
+  };
+  return (
+    <Form
+      initialValuesOfSignIn={initialValuesOfSignIn}
+      type="SignIn"
+      signInSchema={signInSchema}
+      onFormSubmit={handleButtonClick}
+    />
+  );
 };
 
 export default Signin;
