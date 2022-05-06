46b438c5b7b0383f2b3898dc68d338d99b7af38f
Hussain Golwala <hussain.golwala@estatic-infotech.com>
2022-05-06 11:26:48 +0530

    reset password api

--- pages/user/resetpassword.tsx
+++ pages/user/resetpassword.tsx
@@ -8,55 +8,39 @@ interface InitialProps {
 }
 
 const ResetPassword: NextPage = () => {
-  const initialValuesOfResetPassword: InitialProps = { password: "", newPassword: "" };
+  const initialValuesOfResetPassword: InitialProps = {
+    password: "",
+    newPassword: "",
+  };
   const ResetPasswordSchema = yup.object({
-    password: yup
-      .string()
-      .required("Password is required."),
-      // .matches(
-      //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-      //   "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
-      // ),
-      newPassword: yup
-      .string()
-      .required('New password is required.')
-      // .matches(
-      //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-      //   "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
-      // ),
+    password: yup.string().required("Password is required."),
+    // .matches(
+    //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+    //   "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    // ),
+    newPassword: yup.string().required("New password is required."),
+    // .matches(
+    //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+    //   "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    // ),
   });
   const handleResetClick = (data:any) => {
-    // console.log("Password Changed Successfully");
-    // fetch("/api/users/resetpassword", {
-		// 	method: "PUT",
-		// 	headers: {
-		// 		"Content-Type": "application/json",
-		// 	},
-		// 	body: JSON.stringify(data),
-		// })
-		// 	.then((res) => {
-		// 		return res.json();
-		// 	})
-		// 	.then((result) => {
-		// 		alert(result.message)
-		// 	})
-		// 	.catch((e) => {
-		// 		console.log(e);
-		// 	});
-    fetch('/api/users/resetpassword', {
-  method: 'PUT', // or 'PUT'
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({id:1,password:'1234567'}),
-})
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', {id:1,password:'1234567'});
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+
+    let token = localStorage.getItem("token");
+    fetch(`/api/users/resetPassword?token=${token}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({data}),
+    })
+      .then((response) => response.json())
+      .then((id) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
   return (
     <Form
