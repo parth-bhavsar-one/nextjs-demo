6a56a9fcacfc30d33a64d88c0a65c26c121a9f58
Parth Bhavsar <parth.bhavsar@theonetechnologies.co.in>
2022-05-06 11:14:15 +0530

    account activated mail

--- pages/api/auth/verify.ts
+++ pages/api/auth/verify.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { generateToken, verifyToken } from '../../../utils/common'
+import { accountActivated } from '../../../utils/mailHelper'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
@@ -19,6 +20,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     isVerified: true
                 })
             }).then(res => res.json()).then((result) => {
+
+                accountActivated({...result, url: "http://localhost:3000/auth/signin"})
+
                 res.status(200).json(decoded)
             }).catch(err => res.status(500).json({}))
         }).catch(err => res.status(500).json({}))
--- utils/mailHelper.ts
+++ utils/mailHelper.ts
@@ -42,3 +42,35 @@ export const verifyEmail = (data: any) => {
   }
 
 }
+
+
+export const accountActivated = (data: any) => {
+
+    try {
+
+      return new Promise((resolve, reject) => {
+
+        fs.readFile(__dirname + '/../templates/verifyEmail.html', 'utf-8', async (error: any, html: any) => {
+
+          if (error) reject(error)
+
+          html = html.replace("@@URL@@", data.url)
+
+          let mail = {
+            from: process.env.EMAIL,
+            to: data.email,
+            subject: 'Verify your email',
+            html: html,
+          }
+
+          let response = await transporter.sendMail(mail);
+          resolve(response)
+
+        })
+      })
+
+    } catch (error) {
+      throw error
+    }
+
+  }
